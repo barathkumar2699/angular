68c8feda8868652782ba1a7a53598567c853ca44
Barath <barath6.99@gmail.com>
2019-10-26 20:02:03 +0530

    Promises part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,9 +23,13 @@ export class HomeComponent implements OnInit {
     ) { }
 
   ngOnInit() {
-    this.dish = this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionservice.getFeaturedPromotion();
-    this.leader=this.leaderService.getFeaturedLeader();
+    //this.dish = 
+    this.dishservice.getFeaturedDish()
+    .then(dish => this.dish = dish);
+    this.promotionservice.getFeaturedPromotion()
+    .then(promotion => this.promotion = promotion);
+    this.leaderService.getFeaturedLeader()
+    .then(leader => this.leader = leader);
   }
 
 }
