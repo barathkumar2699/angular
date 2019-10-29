a9b9d7f9b64ec0200118b207e6af787ee8cda46e
Barath <barath6.99@gmail.com>
2019-10-29 20:38:38 +0530

    Assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material/slider';
 
 
 
@@ -76,7 +77,8 @@ import {  MatSidenavModule, MatIconModule } from "@angular/material";
     ReactiveFormsModule,
     MatProgressSpinnerModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatSliderModule
   ],
   providers: [DishService,
   PromotionService,
