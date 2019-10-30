d095bc4c884c56264bfaba77b1f5d85cfb79437b
Barath <barath6.99@gmail.com>
2019-10-30 12:18:07 +0530

    Http part 1 continued

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -4,22 +4,26 @@ import { PROMOTIONS } from '../shared/promotions';
 import {of,Observable} from 'rxjs';
 import {delay} from 'rxjs/operators';
 
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getPromotions(): Observable<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000));
+    return this.http.get<Promotion[]>(baseURL + "promotions");
   }
 
   getPromotion(id: number): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promo) => (promo.id === id.toString()))[0]).pipe(delay(2000));
+    return this.http.get<Promotion>(baseURL + "promotions" + id);
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
+    return this.http.get<Promotion[]>(baseURL + "promotions").pipe(map(promotions=>promotions[0]));
   }
 }
